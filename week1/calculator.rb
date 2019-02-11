c0c7dd23e74b38f81f3ce6707ddc369514f9e238
ascbr <adilio.scbr@gmail.com>
2019-02-11 16:15:55 -0600

    calculator...

---
+++ week1/calculator.rb
@@ -0,0 +1,33 @@
+puts 'CALCULATOR'
+
+begin
+  puts 'Insert the firts number'
+  n1 = gets.chomp
+end until n1.to_f != 0
+
+begin
+  puts 'Insert the second number'
+  n2 = gets.chomp
+end until n2.to_f != 0
+begin
+  puts "Seleccionar opcion \n"+
+    "1- Sum\n"+
+    "2- Rest\n"+
+    "3- Mult\n"+
+    "4- Div\n"
+
+  op = gets.chomp
+end until op.to_i != 0
+
+case op.to_i
+  when 1
+    puts "result #{n1}+#{n2} = #{n1.to_f+n2.to_f}"
+  when 2
+    puts "result #{n1}-#{n2} = #{n1.to_f-n2.to_f}"
+  when 3
+    puts "result #{n1}*#{n2} = #{n1.to_f*n2.to_f}"
+  when 4
+    puts "result #{n1}/#{n2} = #{n1.to_f/n2.to_f}"
+  else
+    puts 'opcion no valida'
+end
