8b9d38dc12396db44e648f12c14b29688aa01bf0
ascbr <adilio.scbr@gmail.com>
2019-02-27 14:42:47 -0600

    mocks

--- week4/doubles/stubs-with-constraints/spec/dashboard_spec.rb
+++ week4/doubles/stubs-with-constraints/spec/dashboard_spec.rb
@@ -9,15 +9,9 @@ describe Dashboard do
       visible_posts = double('visible_posts')
       allow(posts).to receive(:visible_to).with(user).and_return(visible_posts)
 
-      result = dashboard.posts
       result = Dashboard.new(posts: posts, user: user).posts
 
-      expect(result.map(&:title)).to match_array(%w(
-        published_one
-        published_two
-        visible_one
-        visible_two
-      ))
+      expect(result).to eq(visible_posts)
     end
   end
 end
